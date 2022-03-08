0d2066c0cac62a1672cec4d0e684d36192a06476
Aravind kanna <kanna007.ka@gmail.com>
2022-03-08 12:39:30 +0530

    Sample&Script

---
+++ DemoApp/DemoApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void click(object sender, EventArgs e)
+        {
+            returntype.Text = (Convert.ToString( Firstname.Text) + Convert.ToString(Lastname.Text)).ToString();
+        }
+
+        private async void Nextpage1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoApp.NextPage_1());
+        }
+
+        private async void Mapcontrolpage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoApp.Page1());
+        }
+    }
+}
---
+++ DemoApp/DemoApp/NextPage_1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NextPage_1 : ContentPage
+    {
+        public NextPage_1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Prepage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
